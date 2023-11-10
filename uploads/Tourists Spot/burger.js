545cdd5228ee29f91762c52cb6ec0e61eb7ac2c0
Regs152001 <regorpaiman0@gmail.com>
2023-11-10 14:07:10 +0800

    Add files via upload

---
+++ uploads/Tourists Spot/burger.js
@@ -0,0 +1,6 @@
+const navbar-toggler-icon = document.getElementById('navbar-toggler-icon');
+const navLinks = document.querySelector('.nav-link');
+
+navbar-toggler-icon.addEventListener('click', () => {
+    nav-link.classList.toggle('active');
+})
