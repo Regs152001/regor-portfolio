68b6c69350645ec6f6a27ddd3fb72e3ab248c689
Regs152001 <regorpaiman0@gmail.com>
2024-03-02 03:22:51 +0800

    Delete uploads directory

--- uploads/Tourists Spot/burger.js
+++
@@ -1,6 +0,0 @@
-const navbar-toggler-icon = document.getElementById('navbar-toggler-icon');
-const navLinks = document.querySelector('.nav-link');
-
-navbar-toggler-icon.addEventListener('click', () => {
-    nav-link.classList.toggle('active');
-})
